75e059c4f005dd12f9d1d58c6c265aae74e5a117
“dinnu247” <dineshkumar.icon@gmail.com>
2021-03-09 17:49:18 +0530

    [dinesh] added dictionary arithmeticoperations.sh

--- arithmeticoperations.sh
+++ arithmeticoperations.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+declare -A dic
 read -p "enter the number" a
 read -p "enter the number" b
 read -p "enter the number" c
@@ -6,3 +7,4 @@ s1=$(( a+b*c ))
 s2=$(( a*b+c ))
 s3=$(( c+a/b ))
 s4=$(( a%b+c ))
+dic=([0]=$s1 [1]=$s2 [2]=$s3 [4]=$s4)
