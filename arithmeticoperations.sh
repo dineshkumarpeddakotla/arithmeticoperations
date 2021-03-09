dfba65a0663f80bc03ca288e8ae1f427125dc8ec
“dinnu247” <dineshkumar.icon@gmail.com>
2021-03-09 18:39:54 +0530

    [dinesh] added array arithmeticoperations.sh

--- arithmeticoperations.sh
+++ arithmeticoperations.sh
@@ -1,5 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
 declare -A dic
+declare -a arr
 read -p "enter the number" a
 read -p "enter the number" b
 read -p "enter the number" c
@@ -7,4 +8,5 @@ s1=$(( a+b*c ))
 s2=$(( a*b+c ))
 s3=$(( c+a/b ))
 s4=$(( a%b+c ))
-dic=([0]=$s1 [1]=$s2 [2]=$s3 [4]=$s4)
+dic=([0]=$s1 [1]=$s2 [2]=$s3 [3]=$s4)
+arr=(${dic[@]})
